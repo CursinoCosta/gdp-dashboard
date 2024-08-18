e22503d25d38bdb03d21988d68be5f014a21bfe4
CursinoCosta <105945436+CursinoCosta@users.noreply.github.com>
2024-08-18 21:11:35 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,27 @@ if opcode == "Aulas Online":
     with st.container():
         st.write("---")
 
-        st.write("Vemos inicialmente a comparação das taxas populacionais e taxas de inscrição por região.")
+        st.write("Limpeza e organização dos dados\n\nAnteriormente, a base de dados possuia muitas colunas, mas, para fins de otimizações das operações feitas no dataframe, foram selecionados apenas as colunas:")
+        st.write("- TP_ST_CONCLUSAO = 2: Conclusão do Ensino Médio no mesmo ano da aplicação do exame")
+        st.write("- TP_PRESENCA_MT = 1: Presença na prova de Matemática")
+        st.write("- TP_PRESENCA_CN = 1: Presença na prova de Ciências da Natureza")
+        st.write("- TP_PRESENCA_LC = 1: Presença na prova de Linguagens")
+        st.write("- TP_PRESENCA_CH = 1: Presença na prova de Ciências Humanas")
+        st.write("Dessa forma, os resultados da base não serão influenciados por alunos que faltaram em alguma prova ou que já tiveram anteriormente a oportunidade de fazer o terceiro ano do Ensino Médio sem a pandemia.")
+        st.write("Além disso, também foi feito o mapeamento das 18 faixas de renda existentes para apenas 5 classes sociais diferentes (A, B, C, D, E) em que a classe social A possui mais recursos financeiros.")
+        st.write("##### Classe E = 0 até 1 salário mínimo.")
+        st.write("##### Classe D = 1 até 3 salários mínimos.")
+        st.write("##### Classe C = 3 até 6 salários mínimos.")
+        st.write("##### CLasse B = 6 até 10 salários mínimos.")
+        st.write("##### Classe A = 10 até 20 salários mínimos.")
+        st.write("Também foi feito a normalização das notas já que para cada ano o ENEM possui uma nota máxima e mínima diferente. Dessa forma, um ano com nota máxima maior não teria vantagem em relação a um ano com nota máxima menor.")
+        
+        
+        st.write("---")
+
+        
+        st.write("A partir da geração da frequências das medias totais para cada ano, percebe-se que todas elas seguem uma distribuição com uma leve assimetria à esquerda.\n\nAlém disso, é interessante observar que, para todas as classes sociais, o ano 2020 possui uma altura menor em relação ao ano de 2022. Isso provavelmente se deve ao fato de que no ano de 2022 ocorreu recorde de abstenção, ou seja, menos alunos foram fazer a prova.")
+
 
         st.image("imagens igor/1.png")
 
@@ -72,31 +92,32 @@ if opcode == "Aulas Online":
     with st.container():
         st.write("---")
 
-        st.write("Vemos inicialmente a comparação das taxas populacionais e taxas de inscrição por região.")
-
         st.image("imagens igor/3.png")
 
+        st.write("Nesse plot foi comparado a média total para cada classe social por ano.")
+        st.write("E, como esperado, os alunos com classes sociais mais altas obtiveram uma nota média maior em relação as classes sociais mais baixas e essa tendência se manteve durante todos os anos.")
+        st.write("Ademais, não parece existir um padrão claro, mas, em geral, as médias totais de 2020 e 2022 se mantiveram estáveis ou subiram em relação ao período de 2017-2019.")
+        
+
     with st.container():
         st.write("---")
-
-        st.write("Vemos inicialmente a comparação das taxas populacionais e taxas de inscrição por região.")
-
         st.image("imagens igor/4.png")
 
-
-    with st.container():
-        st.write("---")
-
-        st.write("Vemos inicialmente a comparação das taxas populacionais e taxas de inscrição por região.")
+        st.write("Também foi gerado boxplots das notas de cada classe social. E também não foi possível")
 
         st.image("imagens igor/5.png")
 
+        
 
     with st.container():
         st.write("---")
 
-        st.write("Vemos inicialmente a comparação das taxas populacionais e taxas de inscrição por região.")
+        st.subheader("Análise de Intervalos de Confiança utilizando Bootstrap\n\nCom limiar de significância de 5%")
 
+        st.write("Foram gerados distribuições bootstrap das médias totais normalizadas para cada classe social em diferentes períodos (2017-2019, 2020 e 2022).")
+        st.write("E, a partir deles, podemos inferir com 95% de certeza, a partir de sua análise visual, há diferenças estatisticamente significativas entre as médias dos períodos comparados (2017-2019 com 2020 e 2017-2019 com 2022), dado que nenhum intervalo de confiança se sobrepõe entre os períodos testados. Ou seja, posso dizer com 95% de confiança que essas médias são diferentes.")
+        st.write("Além disso, é interessante observar que o intervalo de confiança é bem pequeno (4 casas decimais), isso se deve principalmente a nossa amostra ser a própria população e, consequentemnete, a lei dos grande números atua para esse intervalo ser quase que insignificante.")
+        
         st.image("imagens igor/6.png")
 
 
@@ -113,14 +134,6 @@ if opcode == "Aulas Online":
         st.write("Vemos inicialmente a comparação das taxas populacionais e taxas de inscrição por região.")
 
         st.image("imagens igor/12.png")
-
-
-    with st.container():
-        st.write("---")
-        uploaded_file = st.file_uploader("imagens igor/reg.txt")
-        if uploaded_file:
-            for line in uploaded_file:
-                st.write(line)
 
     with st.container():
         st.write("---")
